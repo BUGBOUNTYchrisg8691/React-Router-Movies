e3c3b4704ad1028e046313be385693eccd00e8da
Christopher Girvin <bugbountychrisg8691@gmail.com>
2020-10-10 23:10:53 -0400

    MovieCard component done/stretch

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useParams } from 'react-router-dom';
+import MovieCard from "./MovieCard"
 
 export default function Movie(props) {
   const [movie, setMovie] = useState();
@@ -35,27 +36,31 @@ export default function Movie(props) {
     return <div>Loading movie information...</div>;
   }
 
-  const { title, director, metascore, stars } = movie;
+  // const { title, director, metascore, stars } = movie;
 
   return (
-    <div className="save-wrapper">
-      <div className="movie-card">
-        <h2>{title}</h2>
-        <div className="movie-director">
-          Director: <em>{director}</em>
-        </div>
-        <div className="movie-metascore">
-          Metascore: <strong>{metascore}</strong>
-        </div>
-        <h3>Actors</h3>
+    <MovieCard movie={ movie } stars={ true } saveMovie={ saveMovie } />
+  )
 
-        {stars.map(star => (
-          <div key={star} className="movie-star">
-            {star}
-          </div>
-        ))}
-      </div>
-      <div className="save-button" onClick={ saveMovie }>Save</div>
-    </div>
-  );
+  // return (
+  //   <div className="save-wrapper">
+  //     <div className="movie-card">
+  //       <h2>{title}</h2>
+  //       <div className="movie-director">
+  //         Director: <em>{director}</em>
+  //       </div>
+  //       <div className="movie-metascore">
+  //         Metascore: <strong>{metascore}</strong>
+  //       </div>
+  //       <h3>Actors</h3>
+
+  //       {stars.map(star => (
+  //         <div key={star} className="movie-star">
+  //           {star}
+  //         </div>
+  //       ))}
+  //     </div>
+  //     <div className="save-button" onClick={ saveMovie }>Save</div>
+  //   </div>
+  // );
 }
